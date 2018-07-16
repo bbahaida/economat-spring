7dd26af20b04807ee9adf7ad8ce2feb36ba1aa61
Brahim Bahaida <bbahieda@gmail.com>
2018-07-16 22:26:55 +0000

    initialize the project

---
+++ src/main/java/com/bahaida/economasnim/EconomaSnimApplication.java
@@ -0,0 +1,12 @@
+package com.bahaida.economasnim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EconomaSnimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EconomaSnimApplication.class, args);
+	}
+}
